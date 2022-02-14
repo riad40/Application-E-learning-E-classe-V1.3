eff0cd31aa92097b33b306c6929adb1350e0c0f2
RIAD Abdelaziz <riadabdelaziz40@gmail.com>
2022-02-14 23:01:12 +0100

    link the lougout script & make admin name & users card dynamique

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,9 +1,13 @@
       <!-- Page Sidebar -->
+      <?php 
+      include 'functions.php'; 
+      include 'login-script.php';
+      ?>
       <div id="sidebar">
 
         <img src="./images/E-class.svg" class="ps-3 py-3" alt="E-class" />
         <img src="./images/ycode.png" class="w-50 d-block mx-auto mt-5" alt="" />
-        <h2 class="text-center fs-3 pt-3">Admin Name</h2>
+        <h2 class="text-center fs-3 pt-3"><?php echo $_SESSION["fname"];  ?></h2>
         <p class="text-center text-info">Admin</p>
 
         <div class="text-center px-4 mt-5">
@@ -11,7 +15,7 @@
           <a href="./Dashboard.php" class="d-block nav-link text-dark my-3 <?php if(basename($_SERVER['REQUEST_URI']) == 'Dashboard.php') echo 'btn btn-info'; ?>"><img class="pe-2"
               src="./images/house-door.svg" alt="house-door" />Home</a>
 
-          <a href="Course.php" class="d-block nav-link text-dark my-3 <?php if(basename($_SERVER['REQUEST_URI']) == 'Course.php') echo 'btn btn-info'; ?>"><img class="pe-2" src="./images/bookmark.svg"
+          <a href="./Course.php" class="d-block nav-link text-dark my-3 <?php if(basename($_SERVER['REQUEST_URI']) == 'Course.php') echo 'btn btn-info'; ?>"><img class="pe-2" src="./images/bookmark.svg"
               alt="bookmark" />Course</a>
 
           <a href="./Student.php" class="d-block nav-link text-dark my-3 <?php if(basename($_SERVER['REQUEST_URI']) == 'Student.php') echo 'btn btn-info'; ?>"><img class="pe-2"
@@ -26,7 +30,7 @@
           <a href="#" class="d-block nav-link text-dark my-3"><img class="pe-2" src="./images/sliders.svg"
               alt="sliders" />Settings</a>
 
-          <a href="./index.php" class="nav-link text-center mt-5 text-dark">Log out
+          <a href="includes/logout.php" class="nav-link text-center mt-5 text-dark">Log out
             <img src="./images/box-arrow-right.svg" class="ps-3" alt="bi-box-arrow-right" /></a>
 
         </div>
