b9cb419c77cf4d7c94e3e9445e1450cf7ea575ce
RIAD Abdelaziz <riadabdelaziz40@gmail.com>
2022-02-14 23:04:56 +0100

    set the time of lougout to 24h

--- includes/time-logout.php
+++ includes/time-logout.php
@@ -1,6 +1,6 @@
 
 <?php 
-    if(time() - $_SESSION['login_time'] > 10000000000) { 
+    if(time() - $_SESSION['login_time'] > 86400) { 
         session_destroy();
         header('Location:  index.php'); 
         exit;
